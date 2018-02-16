6c7b7d1ed2d1c4ec06900da0f0b3476d9058002c
horazio <hfusco@stuy.edu>
2018-02-15 20:48:41 -0500

    further error testing

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -24,17 +24,18 @@ public class KnightBoard{
 		    ans += board[r][c] + " ";
 		}
 	    }
-	    ans += "\n";
+	    ans += "\n  ";
 	}
-	return ans;
+	return " \n " + ans + " \n " + count;
     }
 
     private boolean knightMove(int row, int col, int current){
         int[][] coords = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, -1}, {2, 1}, {-2, 1}, {-2, -1}};
         if(current == board.length * board[0].length){
             count++;
+            board[row][col] = current;
             System.out.println(this);
-            System.out.println(count);
+            //System.out.println(count);
             return true;
         }
         for(int i = 0; i < coords.length; i ++){
@@ -44,7 +45,11 @@ public class KnightBoard{
                 if(board[rowAt][colAt] == 0){
                     board[row][col] = current;
                     //System.out.println(this);
-                    knightMove(rowAt, colAt, current + 1);
+                    
+                    if(knightMove(rowAt, colAt, current + 1)){
+                        return true;
+                    }   
+                    
                     board[rowAt][colAt] = 0;
                 }
             }catch(IndexOutOfBoundsException e){
@@ -57,10 +62,29 @@ public class KnightBoard{
     // 5 by 5 at 0 0 is 304, 5 by 5 all countSol 1728
 
     public static void main(String[] args){
-	KnightBoard a = new KnightBoard(5, 5);
-	System.out.println(a.knightMove(1, 3, 1));
-    System.out.println(a.count);
+    KnightBoard a = new KnightBoard(7, 8); 
+    //int co = 0;
+    
+    for(int r = 0; r < a.board.length; r++){
+	    for(int c = 0; c < a.board[0].length; c++){
+            //System.out.println( r + "  " + c);
+            //System.out.println();
+           // a.knightMove(r, c, 1);
+           // a.board = new int[6][6];
+            //System.out.println(a);
+            //System.out.println(a.count);
+            //System.out.println();
+          //  co += a.count;
+	    }
+	} 
+
+        
+   // System.out.println(co);
+    //System.out.println(a.count);
 	//System.out.println(a);
+    //a.count = 0;
+    //a.knightMove(0, 0, 1);
+    System.out.println(a.knightMove(0, 0, 1));
 	
     }
 
