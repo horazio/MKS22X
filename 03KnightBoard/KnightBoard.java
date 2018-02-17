27de79df4f5233e6fd199d9b68c37e4fd55a5e5c
horazio <hfusco@stuy.edu>
2018-02-16 21:39:53 -0500

    fixed indentation errors

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -4,29 +4,28 @@ public class KnightBoard{
     private int count;
 
     public KnightBoard(int startingRows, int startingCols){
-	if(startingRows < 0 || startingCols < 0){
-	    throw new IllegalArgumentException();
-	}
-	board = new int[startingRows][startingCols];
-
+        if(startingRows < 0 || startingCols < 0){
+            throw new IllegalArgumentException();
+        }
+        board = new int[startingRows][startingCols];
     }
 
 
     public String toString(){
-	String ans = "";
-	for(int r = 0; r < board.length; r++){
-	    for(int c = 0; c < board[0].length; c++){
-		if(board[r][c] == 0){
-		    ans += " _ ";
-		}else if(board[r][c] < 10){
-		    ans += " " + board[r][c] + " ";
-		}else{
-		    ans += board[r][c] + " ";
-		}
-	    }
-	    ans += "\n  ";
-	}
-	return " \n " + ans + " \n " + count;
+        String ans = "";
+        for(int r = 0; r < board.length; r++){
+            for(int c = 0; c < board[0].length; c++){
+                if(board[r][c] == 0){
+                    ans += " _ ";
+                }else if(board[r][c] < 10){
+                    ans += " " + board[r][c] + " ";
+                }else{
+                    ans += board[r][c] + " ";
+                }
+            }   
+            ans += "\n  ";
+        }
+        return " \n " + ans + " \n " + count;
     }
 
     private boolean knightMove(int row, int col, int current){
@@ -45,11 +44,7 @@ public class KnightBoard{
                 if(board[rowAt][colAt] == 0){
                     board[row][col] = current;
                     //System.out.println(this);
-                    
-                    if(knightMove(rowAt, colAt, current + 1)){
-                        return true;
-                    }   
-                    
+                    knightMove(rowAt, colAt, current + 1);               
                     board[rowAt][colAt] = 0;
                 }
             }catch(IndexOutOfBoundsException e){
@@ -62,7 +57,7 @@ public class KnightBoard{
     // 5 by 5 at 0 0 is 304, 5 by 5 all countSol 1728
 
     public static void main(String[] args){
-    KnightBoard a = new KnightBoard(4, 6); 
+    KnightBoard a = new KnightBoard(5, 5); 
     //int co = 0;
     
     for(int r = 0; r < a.board.length; r++){
