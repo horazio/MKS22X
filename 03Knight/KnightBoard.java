fae21eaf63f732298ec634b8a29e5c6e73c79559
horazio <hfusco@stuy.edu>
2018-02-18 20:40:39 -0500

    creating the shell methods for solve and countSol

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -10,6 +10,17 @@ public class KnightBoard{
         board = new int[startingRows][startingCols];
     }
 
+    
+    public boolean isClear(){
+        for(int r = 0; r < board.length; r++){
+            for(int c = 0; c < board[0].length; c++){
+                if(board[r][c] != 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public String toString(){
         String ans = "";
@@ -28,6 +39,22 @@ public class KnightBoard{
         return ans;
     }
 
+    public boolean knight(int row, int col){
+        if(this.isClear()){
+            return solve(row, col, 1);
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    public int countSol(int row, int col){
+        this.knightMove(row, col, 1);
+        int holder = count;
+        count = 0;
+        return holder;
+    }
+    
+    
     private boolean knightMove(int row, int col, int current){
         int[][] coords = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, -1}, {2, 1}, {-2, 1}, {-2, -1}};
         if(current == board.length * board[0].length){
@@ -45,6 +72,35 @@ public class KnightBoard{
                     board[row][col] = current;
                     //System.out.println(this);
                     knightMove(rowAt, colAt, current + 1);               
+                    board[rowAt][colAt] = 0;
+                }
+            }catch(IndexOutOfBoundsException e){
+	    }   
+        }
+	return false;
+    }
+    
+    
+    
+    private boolean solve(int row, int col, int current){
+        int[][] coords = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, -1}, {2, 1}, {-2, 1}, {-2, -1}};
+        if(current == board.length * board[0].length){
+            count++;
+            board[row][col] = current;
+            System.out.println(this);
+            //System.out.println(count);
+            return true;
+        }
+        for(int i = 0; i < coords.length; i ++){
+            try{
+                int rowAt = row + coords[i][0];
+                int colAt = col + coords[i][1];
+                if(board[rowAt][colAt] == 0){
+                    board[row][col] = current;
+                    //System.out.println(this);
+                    if(knightMove(rowAt, colAt, current + 1)){
+                        return true;
+                    }               
                     board[rowAt][colAt] = 0;
                 }
             }catch(IndexOutOfBoundsException e){
@@ -79,7 +135,8 @@ public class KnightBoard{
 	System.out.println(a);
     //a.count = 0;
     //a.knightMove(0, 0, 1);
-    System.out.println(a.knightMove(0, 0, 1));
+    System.out.println(a.knight(0, 0));
+    System.out.println(a.knight(0, 0));
     System.out.println(a);
 	
     }
