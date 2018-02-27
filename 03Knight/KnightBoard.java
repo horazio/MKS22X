8763ef73d55bbc47f5bc310f077d629e1f9772d5
horazio <hfusco@stuy.edu>
2018-02-26 23:57:38 -0500

    KnightBoard now clean and error free

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -2,50 +2,14 @@ public class KnightBoard{
 
     private int[][] board;
     private int count;
-    private int[][] boardRef;
 
     public KnightBoard(int startingRows, int startingCols){
         if(startingRows < 0 || startingCols < 0){
             throw new IllegalArgumentException();
         }
         board = new int[startingRows][startingCols];
-        boardRef = new int[startingRows][startingCols];
-        this.fillBoard();
     }
     
-    private void fillBoard(){
-         int[][] coords = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, -1}, {2, 1}, {-2, 1}, {-2, -1}};
-         for(int r = 0; r < board.length; r++){
-            for(int c = 0; c < board[0].length; c++){
-                for(int i = 0; i < coords.length; i++){
-                    try{
-                        board[r + coords[i][0]][c + coords[i][1]]++;
-                        
-                    }catch(IndexOutOfBoundsException e){
-                    } 
-                }                
-            }
-        }
-    }
-
-    private int[][] nextSpot(int row, int col){
-        int[][] coords = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, -1}, {2, 1}, {-2, 1}, {-2, -1}};
-        int[][] newCoords = new int[8][2];
-        for(int i = 0; i < coords.length; i++){
-            int[] lowest = coords[i];
-            for(int j = i; j < coords.length; j ++){
-                try{
-                    if(boardRef[row + coords[j][0]][col + coords[j][1]] < boardRef[row + lowest[0]][col + lowest[1]]){
-                        lowest = coords[j];
-                    }
-                }catch(IndexOutOfBoundsException e){
-                    
-                }
-            newCoords[i] = lowest;
-            }
-        }
-        return newCoords;
-    }
     
     public boolean isClear(){
         for(int r = 0; r < board.length; r++){
@@ -74,26 +38,77 @@ public class KnightBoard{
         }
         return ans;
     }
+    
+    
+    
+    
+    
+    
 
-    public boolean knight(int row, int col){
+    public boolean solve(int row, int col){
         if(this.isClear()){
-            if(solve(row, col, 1)){
+            if(!(row >= 0 && col >= 0 && row < board.length && col < board[0].length)){
+                throw new IllegalArgumentException();
+            }
+            if(solver(row, col, 1)){
                 return true;
             }else{
                 board = new int[board.length][board[0].length];
                 return false;
             }
         }else{
-            throw new IllegalArgumentException();
+            throw new IllegalStateException();
         }
     }
     
-    public int countSol(int row, int col){
-        this.knightMove(row, col, 1);
-        int holder = count;
-        count = 0;
-        board = new int[board.length][board[0].length];
-        return holder;
+    private boolean solver(int row, int col, int current){
+        int[][] coords = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, -1}, {2, 1}, {-2, 1}, {-2, -1}};
+        if(current == board.length * board[0].length){
+            count++;
+            board[row][col] = current;
+            //System.out.println(this);
+            //System.out.println(count);
+            return true;
+        }
+        for(int i = 0; i < coords.length; i ++){
+            try{
+                int rowAt = row + coords[i][0];
+                int colAt = col + coords[i][1];
+                if(board[rowAt][colAt] == 0){
+                    board[row][col] = current;
+                    //System.out.println(this);
+                    if(solver(rowAt, colAt, current + 1)){
+                        return true;
+                    }               
+                    board[rowAt][colAt] = 0;
+                }
+            }catch(IndexOutOfBoundsException e){
+	    }   
+        }
+      
+        return false;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public int countSolutions(int row, int col){
+        if(this.isClear()){
+            if(!(row >= 0 && col >= 0 && row < board.length && col < board[0].length)){
+                throw new IllegalArgumentException();
+            }
+            this.knightMove(row, col, 1);
+            int holder = count;
+            count = 0;
+            board = new int[board.length][board[0].length];
+            return holder;
+        }else{
+            throw new IllegalStateException();
+        }  
     }
     
     
@@ -124,39 +139,13 @@ public class KnightBoard{
     
     
     
-    private boolean solve(int row, int col, int current){
-        int[][] coords = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, -1}, {2, 1}, {-2, 1}, {-2, -1}};
-        if(current == board.length * board[0].length){
-            count++;
-            board[row][col] = current;
-            //System.out.println(this);
-            //System.out.println(count);
-            return true;
-        }
-        for(int i = 0; i < coords.length; i ++){
-            try{
-                int rowAt = row + coords[i][0];
-                int colAt = col + coords[i][1];
-                if(board[rowAt][colAt] == 0){
-                    board[row][col] = current;
-                    //System.out.println(this);
-                    if(solve(rowAt, colAt, current + 1)){
-                        return true;
-                    }               
-                    board[rowAt][colAt] = 0;
-                }
-            }catch(IndexOutOfBoundsException e){
-	    }   
-        }
-      
-        return false;
-    }
+    
     
     
     // 5 by 5 at 0 0 is 304, 5 by 5 all countSol 1728
 
     public static void main(String[] args){
-    KnightBoard a = new KnightBoard(7, 8); 
+    KnightBoard a = new KnightBoard(5, 5); 
     //int co = 0;
     
     for(int r = 0; r < a.board.length; r++){
@@ -188,8 +177,10 @@ public class KnightBoard{
     //System.out.println(a.nextSpot(2, 1)[5][0] + " : "+ a.nextSpot(2,1)[5][1]);
     //System.out.println(a.nextSpot(2, 1)[6][0] + " : " +a.nextSpot(2,1)[6][1]);
     //System.out.println(a.nextSpot(2, 1)[7][0] + " : " +a.nextSpot(2,1)[7][1]);
-    System.out.println(a);
-	
+    //a.solve(5, 2);
+    
+    //System.out.println(a.countSolutions(0, 0));
+	//System.out.println(a);
     }
 
 }
