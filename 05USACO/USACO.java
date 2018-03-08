dc353bfbfdb9742830e7194a77ada6f4d14aceb5
horazio <hfusco@stuy.edu>
2018-03-07 19:08:31 -0500

    silver solution works

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -62,7 +62,31 @@ public class USACO{
         return -1;
     }
     
+    
+    
+    
+    
+    
+    
+    
+    public static void clear(int[][] field){
+        for(int i = 0; i < field.length; i++){
+            for(int j = 0; j < field[0].length; j++){
+               if(field[i][j] > 0){
+                   field[i][j] = 0;
+               } 
+            }
+        }
+    }
+    
+    
+    
+    
+    
     public static int silver(String filename){
+        int[][] moves = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        
         try{
         File text = new File(filename);
         Scanner inf = new Scanner(text);
@@ -75,58 +99,87 @@ public class USACO{
         }
         
         
-        int[][] field = new int[line1[0]][line1[1]];
+        int[][] field1 = new int[line1[0]][line1[1]];
+        int[][] field2 = new int[line1[0]][line1[1]];
         int seconds = line1[2];
         
         
-        String[] nextL;
-        for(int i = 0; i < field.length; i++){
-            nextL = inf.nextLine().split(" ");
-            for(int j = 0; j < field[0].length; j++){
-                if(nextL[j].charAt(0) == '*' ){
-                    field[i][j] = -1;
+        String nextL;
+        for(int i = 0; i < field1.length; i++){
+            nextL = inf.nextLine();
+            for(int j = 0; j < field1[0].length; j++){
+                if(nextL.charAt(j) == '*' ){
+                    field1[i][j] = -1;
+                    field2[i][j] = -1;
                 }
             }
         }
         
         String[] lineLast = inf.nextLine().split(" ");
-        field[Integer.parseInt(lineLast[0]) - 1][Integer.parseInt(lineLast[1]) - 1] = 'S';
-        field[Integer.parseInt(lineLast[2]) - 1][Integer.parseInt(lineLast[3]) - 1] = 'e';
+        field1[Integer.parseInt(lineLast[0]) - 1][Integer.parseInt(lineLast[1]) - 1] = 1;
         
         
+        int[][] holder;
         for(int i = 0; i < seconds; i++){
             
             
             
+            //for(int a = 0; a < field1.length; a++){
+            //    for(int b = 0; b < field1[0].length; b++){
+                    //if(field[i][j] > 0){
+            //            System.out.print(field1[a][b] + " ");
+                    //} 
+            //    }
+            //    System.out.println();
+            //}
+            //System.out.println();
+            //System.out.println();
+            //System.out.println();
+            //System.out.println();
             
-            for(int j = 0; j < field.length; j++){
-                for(int k = 0; k < field[0].length; k++){
+            
+            
+            
+            for(int j = 0; j < field1.length; j++){
+                for(int k = 0; k < field1[0].length; k++){
+                    if(field1[j][k] != -1){
+                        for(int l = 0; l < moves.length; l++){
+                            try{
+                                int rowInc = moves[l][0];
+                                int colInc = moves[l][1];
+                            
+                                if(field1[j + rowInc][k + colInc] != -1){
+                                    field2[j + rowInc][k + colInc] += field1[j][k];
+                                }
+                            
+                            }catch(IndexOutOfBoundsException e){
+                            
+                            }
                     
-                    if(field[j][k] == )
+                        }
+                    
+                    }
+                    
                     
                 }
             }
             
             
             
+            holder = field2;
+            field2 = field1;
+            field1 = holder;
             
+            
+            
+            USACO.clear(field2);
             
             
         }
         
         
         
-        //for(int i = 0; i < field.length; i++){
-        //    for(int j = 0; j < field[0].length; j++){
-        //        
-        //        System.out.print(field[i][j]);
-        //    }
-        //    System.out.println();
-        //}
-        
-        //System.out.println(depth);
-        
-        
+        return field1[Integer.parseInt(lineLast[2]) - 1][Integer.parseInt(lineLast[3]) - 1];
         }catch(FileNotFoundException e){
             
         }
@@ -136,7 +189,7 @@ public class USACO{
     
     
     public static void main(String[] args){
-        USACO.bronze("data1.dat");
+        //System.out.print(USACO.silver("dataS.dat"));
     }
     
 }
