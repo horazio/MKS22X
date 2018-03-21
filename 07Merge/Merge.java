1f90edaa4b688219c892c226ca7a945cbd1d5d31
horazio <hfusco@stuy.edu>
2018-03-21 18:24:56 -0400

    bug solved: parameters bound to local variables will not retain their value outside the function

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -38,32 +38,14 @@ public class Merge{
     
     public static void mergesort(int[] data){
         int[] data2 = new int[data.length];
-        mergesort(data2, data, 0, data.length - 1);
-        data = data2;
-        
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        mergesort(data2, data, 0, data.length - 1);      
         for(int i = 0; i < data.length; i++){
-            System.out.println(data[i]);
-        }
-        
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        
-        
-        
+            data[i] = data2[i];
+        }      
     }
     
     public static void main(String args[]){	
-        int[] dat2 = {2,3,1,12,21,9,8,7,6,5,4,3,2,1,12,31,342,124,12,2,3,4,3,2,1,2,3,4,3,2,1,2,2,3};
-        //int length = dat2.length;
-        //int[] dat1 = new int[length];
-        //merge(dat1, dat2, 0,1, 2, length - 1); 
-        //mergesort(dat1, dat2, 0, length - 1);
+        int[] dat2 = new ;
         mergesort(dat2);
         
         
