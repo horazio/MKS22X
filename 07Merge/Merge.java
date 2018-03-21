c64c9e94834eeea0d38128ac252fbc9462a82b78
horazio <hfusco@stuy.edu>
2018-03-21 18:17:40 -0400

    mergesort works except for a bug in the shell method

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,36 +1,74 @@
 public class Merge{
+    
     public static void mergesort(int[] data, int[] temp, int start, int end){
-	int midpoint = (start + end) / 2;
-	if(start < end){
-	    int[] holder = temp;
-	    temp = data;
-	    data = holder;
-	    mergesort(temp, data, start, midpoint); 
-	    mergesort(temp, data, midpoint + 1, end);
-	}
-	merge(data, temp, start, midpoint, midpoint + 1, end);
-    }
-
-    public static void merge(int[] data, int[] temp, int start, int mid, int augmid, int end){
-	for(int i = start; i < end; i++){
-	    if(augmid >= end || (start < mid && temp[start] < temp[augmid])){
-            data[i] = temp[start];
-            start++;
-	    }else{
-            data[i] = temp[augmid];
-            augmid++;
+        if(start < end){
+            
+            for(int i = start; i <= end; i++){
+                data[i] = temp[i];
+            }
+            
+            int midpoint = (start + end) / 2;
+            
+            //int[] holder = temp;
+            //temp = data;
+            //data = holder;
+            
+            mergesort(temp, data, start, midpoint); 
+            mergesort(temp, data, midpoint + 1, end);
+            
+            
+            
+            merge(data, temp, start, midpoint, midpoint + 1, end);
         }
-	}
+    }
+
+    
+    public static void merge(int[] data, int[] temp, int start, int mid, int augmid, int end){
+        for(int i = start; i <= end; i++){
+                if(augmid > end || (start <= mid && temp[start] < temp[augmid])){
+                    data[i] = temp[start];
+                    start++;
+                }else{
+                    data[i] = temp[augmid];
+                    augmid++;
+                }
+        }
 
     }
+    
+    public static void mergesort(int[] data){
+        int[] data2 = new int[data.length];
+        mergesort(data2, data, 0, data.length - 1);
+        data = data2;
+        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        for(int i = 0; i < data.length; i++){
+            System.out.println(data[i]);
+        }
+        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        
+        
+        
+    }
+    
     public static void main(String args[]){	
-	int[] dat2 = {6,7,9,10,20,1,2,3,4,5};
-	int length = dat2.length;
-	int[] dat1 = new int[length];
-	//merge(dat1, dat2, 0,4, 5, length); 
-    mergesort(dat2, dat1, 0, length);
-	for(int i = 0; i < length; i++){
-	    System.out.println(dat2[i]);
-	}
+        int[] dat2 = {2,3,1,12,21,9,8,7,6,5,4,3,2,1,12,31,342,124,12,2,3,4,3,2,1,2,3,4,3,2,1,2,2,3};
+        //int length = dat2.length;
+        //int[] dat1 = new int[length];
+        //merge(dat1, dat2, 0,1, 2, length - 1); 
+        //mergesort(dat1, dat2, 0, length - 1);
+        mergesort(dat2);
+        
+        
+        for(int i = 0; i < dat2.length; i++){
+            System.out.println(dat2[i]);
+        }
     }
 }
