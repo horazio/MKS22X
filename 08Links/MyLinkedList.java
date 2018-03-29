c232c8bf73f06383c3c5cb220c52d9625a4693f3
horazio <hfusco@stuy.edu>
2018-03-29 15:30:16 -0400

    fixed toString so it works on empty lists, working on get Integer

--- 08Links/MyLinkedList.java
+++ 08Links/MyLinkedList.java
@@ -21,16 +21,33 @@ public class MyLinkedList{
     }
     
     public String toString(){
-        String ans = "";
-        Node current = start;
-        while(current != end){
+        String ans = "[ ";
+	Node current = start;
+	for(int i = size; i > 0; i--){
             ans += current.toString() + " , ";
             current = current.getNext();
         }
-        ans += current.toString();
-        return ans;
+        return ans + " ]";
     }
 
+    public void clear(){
+	start = null;
+	end = null;
+	size = 0;
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public Integer get(int index){
+	
+	Node current = start;
+	for(int i = index; i > 0; i--){
+	    current = current.getNext();
+	}
+    }
+    
     public static void main(String[] args){
         MyLinkedList list = new MyLinkedList();
         list.add(5);
@@ -38,39 +55,41 @@ public class MyLinkedList{
         list.add(5);
         list.add(3);
         list.add(6);
+	//	list.clear();
         System.out.println(list);
+	System.out.println(list.size());
     }
     
     
     private class Node{
-    Node next;
-    int value;
-    
-    public Node(int data, Node nextData){
-	value = data;
-	next = nextData;
+	Node next;
+	Integer value;
+	
+	public Node(int data, Node nextData){
+	    value = data;
+	    next = nextData;
+	}
+	
+	
+	public Node getNext(){
+	    return next;    
+        }
+	
+	public Integer getValue(){
+	    return value;
+	}
+	public String toString(){
+	    return value + "";
+	}
+	public void setNext(Node nextData){
+	    next = nextData;
+	}
+	
+	public void setValue(int data){
+	    value = data;
+	}
+	
     }
-
-
-    public Node getNext(){
-	return next;
-    }
-
-    public int getValue(){
-	return value;
-    }
-    public String toString(){
-	return value + "";
-    }
-    public void setNext(Node nextData){
-	next = nextData;
-    }
-
-    public void setValue(int data){
-	value = data;
-    }
-    
-}
     
     
 }
