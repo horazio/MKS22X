ac98bfd152ba96ccf2514d213eaa74546cb09cd4
horazio <hfusco@stuy.edu>
2018-04-12 21:45:17 -0400

    wrote first iteration of radixsort, still untested

--- 08Links/Sorts.java
+++ 08Links/Sorts.java
@@ -1,6 +1,21 @@
+import java.util.*;
 public class Sort{
     public static void radixsort(MyLinkedListImproved<Integer> data){
-	
+        MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved<Integer>[10];
+        for(int i = 0; i < ((String) data.max()).length(); i++){
+            for(Integer elem : data){
+                bucket[(elem / Math.pow(10, i)) % 10].add(elem);
+            }
+            data.clear();
+            for(int i = 0; i < bucket.length; i++){
+                data.extend(bucket[i]);
+            }
+        }
+        
+    }
+    
+    public static void main(String[] args){
+        
     }
 
     
