d9efb3dce8d079609ad3529b596f78b96d65be40
horazio <hfusco@stuy.edu>
2018-04-14 11:58:16 -0400

    created a helper function that filters out negatives and sorts them seprately

--- 08Links/Sort.java
+++ 08Links/Sort.java
@@ -1,18 +1,43 @@
 import java.util.*;
 public class Sort{
+    
+    
+    
+    
     public static void radixsort(MyLinkedListImproved<Integer> data){
-	@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved[10];
-	for(int i = 0; i < bucket.length; i++){
-	    bucket[i] = new MyLinkedListImproved<Integer>();
-	}
-        for(int i = 0; i < Integer.toString(data.get(data.max())).length(); i++){
-            for(Integer elem : data){
-                bucket[(elem /( (int) Math.pow(10, i))) % 10].add(elem);
+        MyLinkedListImproved<Integer> negData = new MyLinkedListImproved<Integer>();
+    
+        for(Integer elem : data){
+            if(elem.intValue() < 0){
+                negData.add(Integer.valueOf(elem.intValue() * -1));
+                data.remove(elem);
             }
-            data.clear();
-            for(int j = 0; j < bucket.length; j++){
-                data.extend(bucket[j]);
-	        bucket[j].clear();
+        }
+        
+        radixsortHelper(data);
+        radixsortHelper(negData);
+        
+        for(Integer elem : negData){
+            data.add(0, Integer.valueOf(elem.intValue() * -1));
+        }
+    }
+    
+    public static void radixsortHelper(MyLinkedListImproved<Integer> data){
+        if(data.size() > 0){
+        
+            @SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved[10];
+            for(int i = 0; i < bucket.length; i++){
+                bucket[i] = new MyLinkedListImproved<Integer>();
+            }
+            for(int i = 0; i < Integer.toString(data.get(data.max())).length(); i++){
+                for(Integer elem : data){
+                    bucket[(elem /( (int) Math.pow(10, i))) % 10].add(elem);
+                }
+                data.clear();
+                for(int j = 0; j < bucket.length; j++){
+                    data.extend(bucket[j]);
+                    bucket[j].clear();
+                }
             }
         }
 
@@ -28,13 +53,13 @@ public class Sort{
 	//   What if null data 
 	// 
 	//
-        MyLinkedListImproved<Integer> list = new MyLinkedListImproved<>();
+    MyLinkedListImproved<Integer> list = new MyLinkedListImproved<>();
 	list.add(7134);
 	list.add(232);
-	list.add(332);
-	list.add(432);
+	list.add(-332);
+	list.add(-432);
 	list.add(532);
-	list.add(333145);
+	list.add(-333145);
 	list.add(833);
 	radixsort(list);
 	System.out.println(list);
