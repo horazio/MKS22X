eaed255abae601faab59c71379c0f79653f24619
horazio <hfusco@stuy.edu>
2018-04-17 20:50:56 -0400

    began postfix class

---
+++ 09Stack/Postfix.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Postfix{
+    
+    public static double eval(String data){
+        String[] dataS = data.split(" ");
+        LinkedList<String> stack = new LinkedList<String>();
+        String holder;
+        for(String e : dataS){
+            if(e.equals("+")){
+                stack.push("" + (Double.parseDouble(stack.pop()) + Double.parseDouble(stack.pop())));
+            }else if(e.equals("-")){
+                holder = stack.pop();
+                stack.push("" + (Double.parseDouble(stack.pop()) - Double.parseDouble(holder)));
+            }else if(e.equals("*")){
+                stack.push("" + (Double.parseDouble(stack.pop()) * Double.parseDouble(stack.pop())));
+            }else if(e.equals("/")){
+                holder = stack.pop();
+                stack.push("" + (Double.parseDouble(stack.pop()) / Double.parseDouble(holder)));
+            }else if(e.equals("%")){
+                holder = stack.pop();
+                stack.push("" + (Double.parseDouble(stack.pop()) % Double.parseDouble(holder)));
+            }else{
+                stack.push(e);
+            }
+        }
+        
+        return Double.parseDouble(stack.pop());
+    }
+    
+    
+    public static void main(String[] args){
+        System.out.println(eval("10 2.0 +"));
+    }
+    
+    
+    
+    
+}
