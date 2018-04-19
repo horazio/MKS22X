86b61fcc625bc2dbdd882a4118cb8d50ac4f63cc
horazio <hfusco@stuy.edu>
2018-04-19 15:35:22 -0400

    began a rough implementation of methods, filling in necessary exceptions etc...

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -7,6 +7,7 @@ public class MyDeque<E>{
 	myDeque(10);
     }
 
+    @SuppressWarnings("unchecked")
     public MyDeque(int init){
 	if(init < 0){
 	    throw new IllegalArgumentException();
@@ -14,38 +15,83 @@ public class MyDeque<E>{
 	data = (E()) new Object[init];
 	start = 0;
 	end = 0;
+	size = 0;
     }
 
+    @SuppressWarnings("unchecked")
+    private void resize(){
+	if(size == data.length()){
+	    E[] newData = (E()) new Object[2 * size];
+	    for(int i = 0; i < size; i++){
+		newData[i] = data[(start + i) % size];
+	    }
+	    data = newData;
+	}
+    }
+
+    
     public int size(){
 	return size;
     }
 
     public void addFirst(E elem){
-
+	if(elem == null){
+	    throw new NullPointerException();
+	}
+	resize();
+	start = (start - 1) % size;
+	data[start] = elem;
+	size++;
     }
-
+    
     public void addLast(E elem){
-
+	if(elem == null){
+	    throw new NullPointerException();
+	}
+	resize();
+	end =(end + 1) % size;
+	data[end] = elem;
+	size++;
     }
-
+    
     public E removeFirst(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+
 	
+	E holder = data[start];
+	data[start] = null;
+	start = (start + 1) % size;
+	size--;
+	return holder;
     }
 
     public E removeLast(){
-
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	E holder = data[end];
+	data[end] = null;
+	end = (end - 1) % size;
+	size--;
+	return holder; 
     }
 
+            
     
-
+    public E getFirst(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+    }
     
-
+    public E getLast(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+    }
     
-
     
     
-
-
-
-
 }
