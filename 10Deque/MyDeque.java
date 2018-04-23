73dd4803c3199e1efc2b0204e6833a552f69aae8
horazio <hfusco@stuy.edu>
2018-04-22 20:28:41 -0400

    fixed getLast so it returns the index before the last variable

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -123,7 +123,7 @@ public class MyDeque<E>{
 	if(size == 0){
 	    throw new NoSuchElementException();
 	}
-    return data[end];
+    return data[end - 1];
     }
     
     public static void main(String[] args){
