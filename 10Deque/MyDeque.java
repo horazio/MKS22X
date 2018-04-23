b8d3c3963a811d0ca717e8978ae603bc2cc57643
horazio <hfusco@stuy.edu>
2018-04-22 20:24:27 -0400

    added an empty constructor, used chrystals tests and it passes hers as well

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -16,10 +16,13 @@ public class MyDeque<E>{
 	size = 0;
     }
     
-    
-    //public MyDeque(){
-    //    MyDeque<E>(10);
-    //}
+    @SuppressWarnings("unchecked")
+    public MyDeque(){
+        data = (E[]) new Object[10];
+        start = 0;
+        end = 0;
+        size = 0;
+    }
 
     @SuppressWarnings("unchecked")
     private void resize(){
@@ -124,7 +127,51 @@ public class MyDeque<E>{
     }
     
     public static void main(String[] args){
-    
+        MyDeque<String> a = new MyDeque<String>(), a1 = new MyDeque<String>();
+    ArrayList<String> b = new ArrayList<>();
+
+    int size = Integer.parseInt(args[0]);
+    for(int i = 0; i < size; i++){
+      int temp = (int)(Math.random() * 1000);
+      if(temp % 2 == 0){
+        a.addFirst("" + temp);
+        a1.addFirst("" + temp);
+        b.add(0, "" + temp);
+      }
+      else{
+        a.addLast("" + temp);
+        a1.addLast("" + temp);
+        b.add("" + temp);
+      }
+    }
+
+    int index = 0;
+    boolean hasError = false;
+    String errorEvaluation = "Errors found at these indices: ";
+    for (String x : b){
+      if (!(x.equals(a.getFirst()))){
+        System.out.println("The getFirst() function is incorrect at index " + index);
+        hasError = true;
+      }
+      if (!(x.equals(a.removeFirst()))){
+        System.out.println("There is an error at index " + index);
+        errorEvaluation += index + ", ";
+        hasError = true;
+      }
+      index++;
+    }
+
+
+    if(hasError){
+      errorEvaluation = errorEvaluation.substring(0, errorEvaluation.length() - 2);
+      System.out.println(errorEvaluation);
+      System.out.println("MyDeque: " + a1);
+      System.out.println("Actual Deque: " + b);
+    }
+    else{
+      System.out.println("Your deque is bug-free!");
+    }
+
    
     }
     
