f84ccecad0817f7b14baedf0e3a0b3d9a540bf30
horazio <hfusco@stuy.edu>
2018-05-01 20:36:48 -0400

    fixed a bug in heapsort, it now works on preliminary tests. One of the counters in heapify was initialized at the wrong value

--- 12Heap/Sorts.java
+++ 12Heap/Sorts.java
@@ -2,18 +2,14 @@ public class Sorts{
     private static void heapify(int[] data){
         for(int i = data.length - 1; i >= 0; i--){
             pushdown(data, i, data.length);
-	    for(int j = 0; j < data.length; j++){
-		System.out.print(data[j] + " ");
-	    }
-	    System.out.println();
         }
     }
     
     
     private static void pushdown(int[] data, int i, int end){
-	// if(end - i >= 1){
+	 if(end - i > 1){
             int temp;
-            int newi = 0;
+            int newi = i;
             boolean goOn = true;
             while(newi == 0 || (goOn && data[i] <= data[newi])){
                 swap(data, i, newi);
@@ -30,7 +26,7 @@ public class Sorts{
                     }
                 }
             }
-	    //  }
+	      }
     }
     
     public static void swap(int[] data, int index1, int index2){
@@ -41,21 +37,26 @@ public class Sorts{
     
     public static void heapsort(int[] data){
         heapify(data);
+        
         for(int pos = data.length - 1; pos > 0; pos--){
+            System.out.print("Asfasdf");
             swap(data, 0, pos);
             pushdown(data, 0, pos);
         }
     }
     
     public static void main(String[] args){
-        int[] a = {1,1,1,1,1,1,1,3,1,1,1,1};
-	MyHeap<Integer> heap = new MyHeap<Integer>(true);
-
-	  heapify(a);
-	  //	pushdown(a, 0, a.length);
+        int[] a = {1,1,1,1,1,1,1,3,1,1, 234,325,145,513341,13,45,13,13,3,3,31,2,35,13,34,1,57,27,652,624,43,22,1,1};
+	    MyHeap<Integer> heap = new MyHeap<Integer>(true);
         for(int i : a){
-	    heap.add(i);
-            System.out.println(i);
+            System.out.print(i + " ");
+        }
+	    heapsort(a);
+        System.out.println();
+	    //	pushdown(a, 0, a.length);
+        for(int i : a){
+	        //heap.add(i);
+            System.out.print(i + " ");
         }
 
 	//	System.out.println(heap);
